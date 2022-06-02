a44887c1ebd05cdbb6fdae8befd2cf3d435e4bd6
tavanvinh172@gmail.com <tavanvinh172@gmail.com>
2022-06-03 00:43:53 +0700

    thêm dtgridview

--- QL_Cafe/NhanVien.Designer.cs
+++ QL_Cafe/NhanVien.Designer.cs
@@ -28,20 +28,124 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_select = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.btn_remove = new System.Windows.Forms.Button();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 32);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(424, 406);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(27, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(160, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Danh sách sản phẩm";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(653, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Hóa đơn";
+            // 
+            // btn_select
+            // 
+            this.btn_select.Location = new System.Drawing.Point(522, 409);
+            this.btn_select.Name = "btn_select";
+            this.btn_select.Size = new System.Drawing.Size(140, 29);
+            this.btn_select.TabIndex = 3;
+            this.btn_select.Text = "Chọn sản phẩm";
+            this.btn_select.UseVisualStyleBackColor = true;
+            this.btn_select.Click += new System.EventHandler(this.btn_select_Click);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3});
+            this.dataGridView2.Location = new System.Drawing.Point(522, 32);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(330, 358);
+            this.dataGridView2.TabIndex = 4;
+            // 
+            // btn_remove
+            // 
+            this.btn_remove.Location = new System.Drawing.Point(712, 409);
+            this.btn_remove.Name = "btn_remove";
+            this.btn_remove.Size = new System.Drawing.Size(140, 29);
+            this.btn_remove.TabIndex = 5;
+            this.btn_remove.Text = "Bỏ chọn sản phẩm";
+            this.btn_remove.UseVisualStyleBackColor = true;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Name";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Quantity";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Price";
+            this.Column3.Name = "Column3";
             // 
             // NhanVien
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(909, 450);
+            this.Controls.Add(this.btn_remove);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.btn_select);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
             this.Name = "NhanVien";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "NhanVien";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn_select;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button btn_remove;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
     }
 }
--- QL_Cafe/NhanVien.cs
+++ QL_Cafe/NhanVien.cs
@@ -7,14 +7,53 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using Newtonsoft.Json;
 namespace QL_Cafe
 {
     public partial class NhanVien : Form
     {
+        private DataTable sanPham = new DataTable();
+        private int n = 0;
         public NhanVien()
         {
             InitializeComponent();
+            CreateSPtable();
+            dataGridView1.DataSource = sanPham;
+        }
+
+        public void CreateSPtable()
+        {
+            if (System.IO.File.Exists("SanPhams.json"))
+            {
+                System.IO.StreamReader reader = new System.IO.StreamReader("SanPhams.json");
+                string jsonStr = reader.ReadToEnd();
+                reader.Close();
+
+                if (jsonStr != "")
+                {
+                    sanPham = JsonConvert.DeserializeObject<DataTable>(jsonStr);
+                }
+            }
+            if (sanPham.Columns.Count == 0)
+            {
+                sanPham.Columns.Add("Name", typeof(string));
+                sanPham.Columns.Add("Price", typeof(string));
+                sanPham.Columns.Add("Quantity", typeof(int));
+            }
+        }
+
+        private void btn_select_Click(object sender, EventArgs e)
+        {
+            foreach(DataGridViewRow row in dataGridView1.Rows)
+            {
+                if(dataGridView1.Rows.Count != n + 1)
+                {
+                    dataGridView2.Rows.Add();
+                    dataGridView2.Rows[n].Cells[0].Value = row.Cells[1].Value.ToString();
+                    dataGridView2.Rows[n].Cells[2].Value = row.Cells[2].Value.ToString();
+                }
+                n++;
+            }
         }
     }
 }
