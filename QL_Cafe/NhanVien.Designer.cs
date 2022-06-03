021b2138a1438995eaad40e5a94453f7cb4bcbbb
ht-nam <hoangthanhnam10@gmail.com>
2022-06-03 11:46:51 +0700

    add "Add SP"

--- QL_Cafe/NhanVien.Designer.cs
+++ QL_Cafe/NhanVien.Designer.cs
@@ -34,20 +34,24 @@
             this.btn_select = new System.Windows.Forms.Button();
             this.dataGridView2 = new System.Windows.Forms.DataGridView();
             this.btn_remove = new System.Windows.Forms.Button();
-            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(12, 32);
             this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
             this.dataGridView1.Size = new System.Drawing.Size(424, 406);
             this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
             // 
             // label1
             // 
@@ -81,15 +85,15 @@
             // 
             // dataGridView2
             // 
+            this.dataGridView2.AllowUserToAddRows = false;
+            this.dataGridView2.AllowUserToDeleteRows = false;
             this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.Column1,
-            this.Column2,
-            this.Column3});
             this.dataGridView2.Location = new System.Drawing.Point(522, 32);
             this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.ReadOnly = true;
             this.dataGridView2.Size = new System.Drawing.Size(330, 358);
             this.dataGridView2.TabIndex = 4;
+            this.dataGridView2.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView2_CellClick);
             // 
             // btn_remove
             // 
@@ -99,27 +103,33 @@
             this.btn_remove.TabIndex = 5;
             this.btn_remove.Text = "Bỏ chọn sản phẩm";
             this.btn_remove.UseVisualStyleBackColor = true;
+            this.btn_remove.Click += new System.EventHandler(this.btn_remove_Click);
             // 
-            // Column1
+            // label3
             // 
-            this.Column1.HeaderText = "Name";
-            this.Column1.Name = "Column1";
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(464, 32);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "SL";
             // 
-            // Column2
+            // numericUpDown1
             // 
-            this.Column2.HeaderText = "Quantity";
-            this.Column2.Name = "Column2";
-            // 
-            // Column3
-            // 
-            this.Column3.HeaderText = "Price";
-            this.Column3.Name = "Column3";
+            this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDown1.Location = new System.Drawing.Point(455, 55);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(48, 26);
+            this.numericUpDown1.TabIndex = 7;
             // 
             // NhanVien
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(909, 450);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.btn_remove);
             this.Controls.Add(this.dataGridView2);
             this.Controls.Add(this.btn_select);
@@ -131,6 +141,7 @@
             this.Text = "NhanVien";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -144,8 +155,7 @@
         private System.Windows.Forms.Button btn_select;
         private System.Windows.Forms.DataGridView dataGridView2;
         private System.Windows.Forms.Button btn_remove;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
     }
 }
--- QL_Cafe/NhanVien.cs
+++ QL_Cafe/NhanVien.cs
@@ -13,16 +13,30 @@ namespace QL_Cafe
     public partial class NhanVien : Form
     {
         private DataTable sanPham = new DataTable();
-        private int n = 0;
+        private DataTable selectedSP = new DataTable();
+        private string ID = "";
         public NhanVien()
         {
             InitializeComponent();
             CreateSPtable();
             dataGridView1.DataSource = sanPham;
+            dataGridView2.DataSource = selectedSP;
+
+            dataGridView2.AutoGenerateColumns = false;
+            dataGridView2.Columns[0].Visible = false;
+
+            //Thêm đoạn code này sau khi xuất hóa đơn để lưu dữ liệu
+            //string jsonStr = JsonConvert.SerializeObject(sanPham);
+            //System.IO.File.WriteAllText("SanPhams.json", jsonStr);
         }
 
         public void CreateSPtable()
         {
+            selectedSP.Columns.Add("ID");
+            selectedSP.Columns.Add("Name");
+            selectedSP.Columns.Add("Price");
+            selectedSP.Columns.Add("QTT");
+
             if (System.IO.File.Exists("SanPhams.json"))
             {
                 System.IO.StreamReader reader = new System.IO.StreamReader("SanPhams.json");
@@ -36,6 +50,7 @@ namespace QL_Cafe
             }
             if (sanPham.Columns.Count == 0)
             {
+                sanPham.Columns.Add("ID",typeof(string));
                 sanPham.Columns.Add("Name", typeof(string));
                 sanPham.Columns.Add("Price", typeof(string));
                 sanPham.Columns.Add("Quantity", typeof(int));
@@ -44,16 +59,86 @@ namespace QL_Cafe
 
         private void btn_select_Click(object sender, EventArgs e)
         {
-            foreach(DataGridViewRow row in dataGridView1.Rows)
+            if (numericUpDown1.Value.ToString() == "0" || ID == "")
             {
-                if(dataGridView1.Rows.Count != n + 1)
+                MessageBox.Show("Bạn chưa chọn sản phẩm hoặc số lượng");
+            } else {
+                bool check = false;
+                foreach (DataRow row in sanPham.Rows)
                 {
-                    dataGridView2.Rows.Add();
-                    dataGridView2.Rows[n].Cells[0].Value = row.Cells[1].Value.ToString();
-                    dataGridView2.Rows[n].Cells[2].Value = row.Cells[2].Value.ToString();
+                    if (ID != "" && ID == row["ID"].ToString())
+                    {
+                        if (Convert.ToInt32(numericUpDown1.Value.ToString()) > Convert.ToInt32(row["Quantity"].ToString()))
+                        {
+                            MessageBox.Show("Vượt quá số lượng tồn kho");
+                        }
+                        else
+                        {
+                            selectedSP.Rows.Add(row["ID"],row["Name"], row["Price"], numericUpDown1.Value.ToString());
+                            row["Quantity"] = Convert.ToInt32(row["Quantity"].ToString()) - Convert.ToInt32(numericUpDown1.Value.ToString());
+                            
+                            dataGridView1.DataSource = sanPham;
+                            dataGridView2.DataSource = selectedSP;
+                            numericUpDown1.Value = 0;
+                            check = true;
+                            ID = "";
+                        }
+                    }
                 }
-                n++;
+                if (check == false)
+                {
+                    MessageBox.Show("Chọn sản phẩm không thành công");
+                }
             }
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            ID = dataGridView1.SelectedCells[0].OwningRow.Cells[0].Value.ToString();
+        }
+
+        private void btn_remove_Click(object sender, EventArgs e)
+        {
+            DataRow dlRow = null;
+            if (ID == "")
+            {
+                MessageBox.Show("Bạn chưa chọn sản phẩm");
+            } else
+            {
+                bool check = false;
+                foreach (DataRow row in selectedSP.Rows)
+                {
+                    if (ID != "" && ID == row["ID"].ToString())
+                    {
+
+                        foreach (DataRow r in sanPham.Rows)
+                        {
+                            if (ID != "" && ID == r["ID"].ToString())
+                            {
+                                r["Quantity"] = Convert.ToInt32(row["QTT"].ToString()) + Convert.ToInt32(r["Quantity"].ToString());
+                            }
+                        }
+
+                        dlRow = row;
+                        dataGridView1.DataSource = sanPham;
+                        check = true;
+                        ID = "";
+                    }
+                }
+                if (check == false)
+                {
+                    MessageBox.Show("Xóa sản phẩm không thành công");
+                } else
+                {
+                    if (dlRow != null)
+                        selectedSP.Rows.Remove(dlRow);
+                }
+            }
+        }
+
+        private void dataGridView2_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            ID = dataGridView1.SelectedCells[0].OwningRow.Cells[0].Value.ToString();
         }
     }
 }
